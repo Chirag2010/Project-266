223c56e53e2b93c3aab9597c55bfdd35ff120213
sahil singh <sinchirag1@gmail.com>
2023-10-23 11:26:59 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+import numpy as np
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    #write the code from here
+    image_file = request.files['file']
+    degree = int(request.form['text'])
+    filename = secure_filename(image_file.filename)
+    image_file.save(os.path.join('static/', filename))
+    image=Image.open(image_file)
+    image_rotation_degree=image.rotate(degree)
+    image_rotation_degree.save(os.path.join('static/', 'rotated_image.jpg'))
+    img_rotate='rotated_image.jpg'
+    return render_template('upload.html', filename=img_rotate)
+
+
+    # Write code for Select option for Gray and Sketch
+    if operation_selection=='gray':
+        file_data=make_grayscale(decode_array_to_img)
+    elif operation_selection=="sketch":
+        file_data=image_sketch(decode_array_to_img)
+    else:
+        print("No image selected!")
+
+
+
+
+
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename=filename))
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
